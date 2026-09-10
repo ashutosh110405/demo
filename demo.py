5d8bca48f831902422674aedc4f8e5068153576d
ashutosh110405 <ashutosh110405@gmail.com>
2026-09-10 14:41:01 +0530

    version 1.0.1

--- demo.py
+++ demo.py
@@ -2,3 +2,5 @@ role = input("Enter u r role: ")
 age = int(input("Enter u r age: "))
 eligible = (role.lower() == "student") and (age < 21)
 print("Eligible:", eligible)
+# version 1.0.1 commit temp
+#demo version
