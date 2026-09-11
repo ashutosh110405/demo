7b6481b448bddd8f9cb36dc7f6765fb4d721d46f
ashutosh110405 <ashutosh110405@gmail.com>
2026-09-11 11:37:01 +0530

    version 1.0.8

--- demo7.py
+++ demo7.py
@@ -4,9 +4,19 @@ b = int(input("Enter Value B: "))
 
 c = int(input("Enter Value C: "))
 
-if a >= b and a >= c:
-    print("Greater value is",a)
-elif b >= c and b >= a:
-    print("Greater value is",b)
+if a > b and a > c:
+    print("Greater value is A : ",a)
+elif b > c and b > a:
+    print("Greater value is B :",b)
 elif c > a:
-    print("Greater value is",c)
+    print("Greater value is C : ",c)
+
+elif a ==  b == c:
+    print("All values are same")
+elif a == b:
+    print("Value A & B are the same")
+elif b == c:
+    print("Value B & C are the same")
+elif c == a:
+    print("Value C & A are the same")
+
