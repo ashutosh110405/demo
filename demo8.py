a3a4e29648a73ea5dcc8c601d05ff07cd61eeb0c
ashutosh110405 <ashutosh110405@gmail.com>
2026-09-11 13:47:37 +0530

    version 1.0.9

---
+++ demo8.py
@@ -0,0 +1,16 @@
+#basic calculator
+#a = int(input("Enter Value 1: "))
+#b = int(input("Enter Value 2: "))
+#
+#add = a + b
+#sub = a - b
+#div = a / b
+#mul = a * b
+#
+#cal = input("MEthods : add, sub, div, mul : ")
+#
+#match cal:
+#    case 1:
+#        print("Addition is : ", a + b)
+#    case 2:
+#        print("Substraction is : ", sub)
