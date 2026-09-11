cdab5bb5f7e4fe3f0eef1955c2d0ca4eb8260993
ashutosh110405 <ashutosh110405@gmail.com>
2026-09-11 11:17:20 +0530

    version 1.0.6

---
+++ demo7.py
@@ -0,0 +1,12 @@
+a = int(input("Enter Value A: "))
+
+b = int(input("Enter Value B: "))
+
+c = int(input("Enter Value C: "))
+
+if a > b:
+    print("A is greater value",a)
+elif b > c:
+    print("B is greater value",b)
+elif c > a:
+    print("C is greater value",c)
