c7d925ff102d997f44415bb10f67ab26c7d7ba24
ashutosh110405 <ashutosh110405@gmail.com>
2026-09-15 14:20:46 +0530

    version2.2

--- loop/loop password.py
+++ loop/loop password.py
@@ -1,3 +1,4 @@
+# check password
 correct_pass = "same_pass"
 not_found = True
 
@@ -9,3 +10,10 @@ while not_found:
         print("Incorrect Please try again.")
 
 print("Password MAtched!")
+
+# print even  number from 0 to 120 only
+
+i = 0
+while i <= 120:
+    i+= 2
+    print(i)
