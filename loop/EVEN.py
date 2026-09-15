b2376f18182bdd418d8260a2bed0f6d408428c9b
ashutosh110405 <ashutosh110405@gmail.com>
2026-09-15 14:24:49 +0530

    version2.3

---
+++ loop/EVEN.py
@@ -0,0 +1,6 @@
+# print even  number from 0 to 120 only
+
+i = 0
+while i <= 120:
+    i+= 2
+    print(i)
--- loop/loop password.py
+++ loop/loop password.py
@@ -10,10 +10,3 @@ while not_found:
         print("Incorrect Please try again.")
 
 print("Password MAtched!")
-
-# print even  number from 0 to 120 only
-
-i = 0
-while i <= 120:
-    i+= 2
-    print(i)
