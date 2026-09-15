59f81fc4f0b266615ba27ecea525e97b6e47d817
ashutosh110405 <ashutosh110405@gmail.com>
2026-09-15 14:02:26 +0530

    version2.1

---
+++ loop/loop password.py
@@ -0,0 +1,11 @@
+correct_pass = "same_pass"
+not_found = True
+
+while not_found:
+    string = input("Enter a string: ")
+    if string == correct_pass:
+        not_found = False
+    else:
+        print("Incorrect Please try again.")
+
+print("Password MAtched!")
--- loop/loop.py
+++ loop/loop.py
@@ -1,3 +1,4 @@
 while True:
     string = input("Enter a string: ")
     print(string)
+    #printing infinite string loop
