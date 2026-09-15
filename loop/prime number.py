10b034b286bb61ffa1945a9a7c15e7a874cd2c34
ashutosh110405 <ashutosh110405@gmail.com>
2026-09-15 14:45:16 +0530

    version2.2

---
+++ loop/prime number.py
@@ -0,0 +1,8 @@
+#print prime number from 1 to 50
+for num in range(1, 51):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
---
+++ loop/table of 10.py
@@ -0,0 +1,3 @@
+#print table of 10
+for i in range(1, 11):
+    print("10 *", i, "=", 10*i)
