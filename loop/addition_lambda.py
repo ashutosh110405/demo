621f3c9d432587b2ec1609a536eb4cf46503eb3c
ashutosh110405 <ashutosh110405@gmail.com>
2026-09-17 10:53:14 +0530

    version2.6.2

---
+++ loop/addition_lambda.py
@@ -0,0 +1,5 @@
+#do addition using lambda
+add = lambda x, y: x + y
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print("Sum of", x, "and", y, "is", add(x, y))
---
+++ loop/find_square_lambda.py
@@ -0,0 +1,4 @@
+#to find square of n value using lambda function and accepting value from user
+square = lambda n: n * n    
+n = int(input("Enter a number: "))
+print("Square of", n, "is", square(n))
